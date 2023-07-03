d267f03a30f5ccb03de113b1af98e3bc3c739fe0
Jonathan Blau <122023139+CoolSandwich20@users.noreply.github.com>
2023-07-03 16:16:06 +0300

    Do Tween in I DUNNO WTF IS EVEN THAT SHTT

--- Minigames/Assets/_Scripts/Main Menu/MainMenuManager.cs
+++ Minigames/Assets/_Scripts/Main Menu/MainMenuManager.cs
@@ -1,10 +1,15 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    [SerializeField] GameObject TitleName;
+    [SerializeField] GameObject MadeBy;
     [SerializeField] PlayMenuManager playMenu;
+    [SerializeField] GameObject PlayButton;
     public void PlayMenu()
     {
         gameObject.SetActive(false);
@@ -13,6 +18,16 @@ public class MainMenuManager : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+    public void Start()
+    {
+
+        MadeBy.transform.DOMoveX(transform.position.x ,2.5f);
+        PlayButton.transform.DOPunchRotation(new(0, 0, 360), 2f);
+    }
+    public void RotateTitleScreen()
+    {
+        TitleName.transform.DOPunchScale(transform.localScale,1f);
     }
 
 }
